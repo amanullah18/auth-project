68a4d162ac3339e72d758dc258e2a30662200128
amanullah18 <amanullahkan6490@gmail.com>
2025-05-03 12:32:41 +0500

    Initial commit: NestJS project

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(data: Partial<User>) {
+    const user = this.repo.create(data);
+    return this.repo.save(user);
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+
+  findById(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+}
