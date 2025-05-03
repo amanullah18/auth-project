a6331c6eaf1bf312b230f6a36a27cc1317d89986
amanullah18 <amanullahkan6490@gmail.com>
2025-05-03 12:32:41 +0500

    Initial commit: NestJS project

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromHeader('x-access-token'),
+      ignoreExpiration: false,
+      secretOrKey: '12364783782738273',
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.username };
+  }
+}
